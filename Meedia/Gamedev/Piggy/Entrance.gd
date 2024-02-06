cf4cdf0d8ff7884c19ef751193b2ae8cfd45ef97
Peeter <123249766+palliks@users.noreply.github.com>
2024-02-06 14:02:27 +0200

    Godot projekt alpha

---
+++ Meedia/Gamedev/Piggy/Entrance.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+var entered = false
+
+func _on_body_entered(_body):
+	entered = true
+	
+
+func _on_body_exited(_body):
+	entered = false
+	
+func _process(_delta):
+	if entered:
+		if Input.is_action_just_pressed("ui_accept"):
+			Global.from_level = get_parent().name
+			
+			get_tree().change_scene_to_file("res://Levels/"+ self.name +".tscn")
+			
--- Meedia/Gamedev/README.md
+++ Meedia/Gamedev/README.md
@@ -1,1 +1,4 @@
 Siia tulevad ainult mänguarendusega seotud failid
+
+Kasutame Godot 4 for Windows
+Download link [Godot Engine 4.2.1](https://github.com/godotengine/godot/releases/download/4.2.1-stable/Godot_v4.2.1-stable_win64.exe.zip)
