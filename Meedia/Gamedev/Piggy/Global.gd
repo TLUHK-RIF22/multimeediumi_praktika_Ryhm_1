a43c03dc11353ba06071c314e865c6cba77229e5
Peeter <123249766+palliks@users.noreply.github.com>
2024-03-13 22:52:23 +0200

    Inventory show on button press
    Kui hoiad Inventory nuppu all "TAB" või "I", siis näed mis sul inventorys asuvad

--- Meedia/Gamedev/Piggy/Global.gd
+++ Meedia/Gamedev/Piggy/Global.gd
@@ -2,4 +2,5 @@ extends Node2D
 
 var from_level
 var audioLevel = 0.0
+@warning_ignore("unused_private_class_variable")
 var _content:Array[Item] = []
--- Meedia/Gamedev/Piggy/Inventory/UIRoot.gd
+++ Meedia/Gamedev/Piggy/Inventory/UIRoot.gd
@@ -1,8 +1,16 @@
 extends CanvasLayer
 
-@onready var inventory_dialog:InventoryDialog = %InventoryDialog
+@onready var inventory_dialog:PanelContainer = %InventoryDialog
 @onready var player:Player = $".."
+var open = false
 
-func _unhandled_input(event):
-	if event.is_action_released("inventory"):
+func _input(event: InputEvent) -> void:
+	if(event.is_action_pressed("inventory") and !open):
+		open = true
 		inventory_dialog.open(player.inventory)
+		
+	if(event.is_action_released("inventory") and open) :
+		open = false
+		inventory_dialog.hide()
+		
+		
