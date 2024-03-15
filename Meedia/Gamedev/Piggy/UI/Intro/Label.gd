397d590dfb25297a214a19ac79943df998a918f4
Peeter <123249766+palliks@users.noreply.github.com>
2024-03-15 21:55:07 +0200

    IntroSkipButton
    skip nupule timeri külge pandud. Kui 3 sekundi jooksul teist korda nupu ei vajuta, siis tekst kaob eest ära ja resetib counteri

--- Meedia/Gamedev/Piggy/UI/Intro/Label.gd
+++ Meedia/Gamedev/Piggy/UI/Intro/Label.gd
@@ -4,10 +4,17 @@ extends Label
 var skip_count = 0
 
 func _physics_process(_delta):
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_cancel"):
 		skip_count +=1
-
+		
+		
 		if skip_count == 1:
 			set_text("Press Enter to Skip")
+			%Timer.start()
 		elif skip_count == 2:
 			get_tree().change_scene_to_packed(MainMenu)
+
+
+func _on_timer_timeout():
+	skip_count = 0
+	set_text("")
