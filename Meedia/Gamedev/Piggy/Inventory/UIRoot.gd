fc36eaaef1f595e2b275e1a1e99c7d9b27348904
Peeter <123249766+palliks@users.noreply.github.com>
2024-03-17 12:01:19 +0200

    Inventory avamine
    Inventory on nüüd avatav ja suletav "inventory" nuppu kasutades (TAB, I ja puldil X) Bug ära parandatud, mille pärast pidi inventory nuppu all hoidma, et Inventory ekraanil näha.

--- Meedia/Gamedev/Piggy/Inventory/UIRoot.gd
+++ Meedia/Gamedev/Piggy/Inventory/UIRoot.gd
@@ -2,15 +2,11 @@ extends CanvasLayer
 
 @onready var inventory_dialog:PanelContainer = %InventoryDialog
 @onready var player:Player = $".."
-var open = false
 
 func _input(event: InputEvent) -> void:
-	if(event.is_action_pressed("inventory") and !open):
-		open = true
-		inventory_dialog.open(player.inventory)
-		
-	if(event.is_action_released("inventory") and open) :
-		open = false
-		inventory_dialog.hide()
-		
-		
+	if(event.is_action_pressed("inventory")):
+		if !inventory_dialog.visible:
+			inventory_dialog.open(player.inventory)
+
+		else:
+			inventory_dialog.hide()
