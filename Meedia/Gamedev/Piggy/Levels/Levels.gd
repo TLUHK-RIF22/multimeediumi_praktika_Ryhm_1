02230e49cd3815dae61b1cf4fc18ee7c539a966e
Peeter <123249766+palliks@users.noreply.github.com>
2024-03-17 10:21:16 +0200

    ItemDuplicationFix
    Leveli laadimisel kontrollitakse, kas Item on juba üles korjatud ja asub su Inventorys ehk kas see asub Global._content array-s. Kui jah, siis seda ei laeta uude levelisse. See muudab Leveli vahel edasi tagasi liikumise paremaks, sest siis ei saa mängija korduvalt sama itemit üles korjata.

--- Meedia/Gamedev/Piggy/Levels/Levels.gd
+++ Meedia/Gamedev/Piggy/Levels/Levels.gd
@@ -5,5 +5,6 @@ extends Node2D
 func _ready():
 	if Global.from_level != null:
 		get_node("Player").set_position(get_node(Global.from_level + "Pos").position)
-		
+		print(Global._content)
+	
 
--- Meedia/Gamedev/Piggy/Pickup/pickup.gd
+++ Meedia/Gamedev/Piggy/Pickup/pickup.gd
@@ -6,6 +6,11 @@ func _ready():
 	var instance = item.scene.instantiate()
 	add_child(instance)
 	
+	#Items are unique, can only be picked up once
+	if item in Global._content:
+		queue_free()
+	
+
 
 func _on_area_2d_body_entered(body):
 	if body.has_method("on_item_picked_up"):
