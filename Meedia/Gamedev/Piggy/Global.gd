16cb1d43080a764172830d045e82fa5ad8504e7d
Peeter <123249766+palliks@users.noreply.github.com>
2024-03-19 12:56:41 +0200

    ItemPickUp
    Uus global muutuja loodud 'itemsPickedUp', mis peab arvestust, mis levelist, milline item on üles korjatud. Kui Item on üles korjatud, siis seda ei laeta enam levelisse. Seda kontrollib 'pickup.gd' script.

--- Meedia/Gamedev/Piggy/Global.gd
+++ Meedia/Gamedev/Piggy/Global.gd
@@ -4,5 +4,5 @@ var from_level
 var audioLevel = 0.0
 @warning_ignore("unused_private_class_variable")
 var _content:Array[Item] = []
-
+var itemsPickedUp :Array = []
 var apple = true
--- Meedia/Gamedev/Piggy/Pickup/pickup.gd
+++ Meedia/Gamedev/Piggy/Pickup/pickup.gd
@@ -1,18 +1,18 @@
 extends Node2D
 
 @export var item:Item
+var itemsPickedUp = Global.itemsPickedUp
 
 func _ready():
 	var instance = item.scene.instantiate()
 	add_child(instance)
 	
 	#Items are unique, can only be picked up once
-	if item in Global._content:
+	if get_parent().name +" " + self.name in Global.itemsPickedUp:
 		queue_free()
 	
-
-
 func _on_area_2d_body_entered(body):
 	if body.has_method("on_item_picked_up"):
 		body.on_item_picked_up(item)
+		itemsPickedUp.append(get_parent().name +" " + self.name)
 		queue_free()
