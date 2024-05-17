90fa172d0cd04b541a16b2b921211a9d59d7b2c3
Peeter <123249766+palliks@users.noreply.github.com>
2024-05-17 19:10:08 +0300

    BoatEscape
    Viimane katsumus. Paadiga põgenemine. Saad liikuda üles, alla ja paremale. vb mõned takistused veel teel ees.

---
+++ Meedia/Gamedev/Piggy/Charecters/Siga/Scenes/playerBoat.gd
@@ -0,0 +1,28 @@
+class_name PlayerBoat
+extends CharacterBody2D
+
+@onready var playerWalkingAudioStream = %AudioStreamPlayer_Walking
+@export var default_speed: float = 80
+var move_speed : float = default_speed
+@export var starting_direction : Vector2 = Vector2(0,0)
+
+@export var idleTimer = false
+var input_direction: Vector2
+
+
+func _physics_process(_delta):
+
+	input_direction = Vector2(
+		Input.get_action_strength("ui_right") #- Input.get_action_strength("ui_left")
+		,
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	)
+
+
+	velocity = velocity.normalized()
+	velocity = input_direction * move_speed
+	
+	
+	# Move and Slide 
+	move_and_slide()
+
